06c4446320c508f573a2f596a939f8aeb78664da
mg8848 <mg@ethsana.org>
2021-08-13 10:03:35 +0800

    add node selector to the navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,13 @@
 import { ReactElement, useCallback } from 'react'
 
 import { createStyles, makeStyles } from '@material-ui/core/styles'
-import { Toolbar, Chip, IconButton } from '@material-ui/core/'
+import { Toolbar, Chip, IconButton, Select, MenuItem } from '@material-ui/core/'
 
 import { Sun, Moon } from 'react-feather'
 import SanaLogoLight from '../assets/sana-logo-light.svg'
 import SanaLogoDark from '../assets/sana-logo-dark.svg'
 import useTheme from '../hooks/useTheme'
+import useApplication from '../hooks/useApplication'
 
 const useStyles = makeStyles(theme =>
   createStyles({
@@ -25,6 +26,7 @@ const useStyles = makeStyles(theme =>
 export default function SideBar(): ReactElement {
   const classes = useStyles()
   const { theme: themeMode } = useTheme()
+  const { nodeApi, nodeApiList, refresh } = useApplication()
 
   // const clickTheme = useCallback(() => {
   //   if (updater && typeof updater === 'function') {
@@ -38,7 +40,25 @@ export default function SideBar(): ReactElement {
         <img src={themeMode === 'light' ? SanaLogoLight : SanaLogoDark} alt="sana logo" width="50" />
       </div>
       <div style={{ display: 'flex', alignItems: 'center' }}>
-        <Chip style={{ marginLeft: '7px' }} size="small" label="xDai" className={classes.network} />
+        {nodeApiList.length > 1 && (
+          <Select
+            value={nodeApi.id}
+            onChange={event => {
+              localStorage.acitve_node_api_key = event.target.value
+              refresh()
+              window.location.reload()
+            }}
+          >
+            {nodeApiList.map(({ id, nodeName, apiHost, debugApiHost }) => {
+              return (
+                <MenuItem key={id} value={id}>
+                  {nodeName || apiHost || debugApiHost}
+                </MenuItem>
+              )
+            })}
+          </Select>
+        )}
+        <Chip style={{ marginLeft: '12px' }} size="small" label="xDai" className={classes.network} />
         {/* <div style={{ width: '100%' }}>
           <div style={{ float: 'right' }}>
             <IconButton style={{ marginRight: '10px' }} aria-label="dark-mode" onClick={clickTheme}>
